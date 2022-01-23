4a85febb1bc93cd7c77387e0dcd7b3be9c0a169b
Anand Varma <achekur@ncsu.edu>
2022-01-23 18:28:23 -0500

    Populated adjectives and added guardrails
    - added command line args and flags
    - populated the adjectives dict
    - added sanity checks for dicts

--- cmd/namegen/main.go
+++ cmd/namegen/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/anandvarma/namegen"
 )
 
 func main() {
+	// Command line flags.
+	lPtr := flag.Int("postfix", 4, "The length of the postfix id in number of characters.")
+	aPtr := flag.Bool("alphanum", false, "If set, the postfix is represented as an alpha-numeric id instead of a decimal number.")
+	flag.Parse()
+
+	// Parse command line arguments.
+	args := flag.Args()
+	if len(args) > 1 {
+		fmt.Println("Unexpected command line arguments. Run 'namegen --help' to see usage.")
+		os.Exit(1)
+	}
+	numIters := 1
+	if len(args) == 1 {
+		if num, err := strconv.Atoi(args[0]); err != nil {
+			fmt.Println("Received bad value of <num-iterations>. Run 'namegen --help' to see usage.")
+			os.Exit(1)
+		} else {
+			numIters = num
+		}
+	}
+
+	// Initialize a nameGen instance with the received options.
 	name_schema := []namegen.DictType{
 		namegen.Adjectives,
 		namegen.Animals,
 	}
-	ngen := namegen.NewWithPostfixId(name_schema, namegen.Numeric /* pIdType */, 4 /* pIdLen */)
-	fmt.Println(ngen.Get())
+	var pIdType namegen.PostfixIdType = namegen.Numeric
+	if *aPtr {
+		pIdType = namegen.AlphaNumeric
+	}
+	ngen := namegen.NewWithPostfixId(name_schema, pIdType, *lPtr /* pIdLen */)
+
+	for i := 0; i < numIters; i++ {
+		fmt.Println(ngen.Get())
+	}
 }
--- namegen_test.go
+++ namegen_test.go
@@ -1,6 +1,7 @@
 package namegen
 
 import (
+	"sort"
 	"strings"
 	"testing"
 )
@@ -98,4 +99,26 @@ func TestSetters(t *testing.T) {
 	if n.pIdLen != 7 {
 		t.Errorf("Unexpected prefix length: %d", n.pIdLen)
 	}
+}
+
+func TestDictionarySanity(t *testing.T) {
+	for i := 0; i < int(numDicts); i++ {
+		d := dicts[i]
+
+		// Check to make sure the dictionary lists are sorted.
+		sortedByName := sort.SliceIsSorted(d, func(i, j int) bool {
+			return d[i] < d[j]
+		})
+		if !sortedByName {
+			t.Errorf("Dictionary %d is not sorted!", i)
+		}
+
+		// Check to make sure there are no duplicates within a dictionary list.
+		for j := 1; j < len(d); j++ {
+			if d[j] == d[j-1] {
+				t.Errorf("Dictionary %d has a duplicate entry: %s", i, d[j])
+			}
+		}
+	}
+
 }
