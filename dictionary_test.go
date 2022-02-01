14d55070e972582e9efe225d9e7d5e7545ba67b6
neverbeenthisweeb <rf.ramadhana@gmail.com>
2022-02-01 21:27:25 +0700

    Add test to spot duplicates across dictionaries
    - Fix #6

--- dictionary_test.go
+++ dictionary_test.go
@@ -6,22 +6,42 @@ import (
 )
 
 func TestDictionarySanity(t *testing.T) {
-	for i := 0; i < int(numDicts); i++ {
-		d := dicts[i]
+	t.Run("ShouldBeSortedAndNotHaveDuplicateOnEachDictionary", func(t *testing.T) {
+		for i := 0; i < int(numDicts); i++ {
+			d := dicts[i]
 
-		// Check to make sure the dictionary lists are sorted.
-		sortedByName := sort.SliceIsSorted(d, func(i, j int) bool {
-			return d[i] < d[j]
-		})
-		if !sortedByName {
-			t.Errorf("Dictionary %d is not sorted!", i)
-		}
+			// Check to make sure the dictionary lists are sorted.
+			sortedByName := sort.SliceIsSorted(d, func(i, j int) bool {
+				return d[i] < d[j]
+			})
+			if !sortedByName {
+				t.Errorf("Dictionary %d is not sorted!", i)
+			}
 
-		// Check to make sure there are no duplicates within a dictionary list.
-		for j := 1; j < len(d); j++ {
-			if d[j] == d[j-1] {
-				t.Errorf("Dictionary %d has a duplicate entry: %s", i, d[j])
+			// Check to make sure there are no duplicates within a dictionary list.
+			for j := 1; j < len(d); j++ {
+				if d[j] == d[j-1] {
+					t.Errorf("Dictionary %d has a duplicate entry: %s", i, d[j])
+				}
 			}
 		}
-	}
+	})
+
+	t.Run("ShouldNotHaveDuplicateAcrossDictionaries", func(t *testing.T) {
+		allDicts := []string{}
+
+		// Append all dictionaries into one
+		for _, v := range dicts {
+			allDicts = append(allDicts, v...)
+		}
+
+		// Check if there is any duplicate in all appended dictionaries
+		isExist := make(map[string]bool)
+		for _, vv := range allDicts {
+			if isExist[vv] {
+				t.Errorf("Duplicate entry across dictionaries: %s", vv)
+			}
+			isExist[vv] = true
+		}
+	})
 }
